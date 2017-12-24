95c7ff5335d0f26adc9d11c6a8726362f0cb9a5b
577453755 <577453755@qq.com>
2017-12-24 16:09:08 +0800

    2017-12-24

---
+++ src/main/java/dt59homework/student.java
@@ -0,0 +1,44 @@
+/**
+ * Project Name:lession1
+ * File Name:student.java
+ * Package Name:lession4
+ * Date:2017年12月24日上午11:14:07
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package lession4;
+
+/**
+ * Description: <br/>
+ * Date: 2017年12月24日 上午11:14:07 <br/>
+ * 
+ * @author 57745
+ * @version
+ * @see
+ */
+public class student {
+    public static void main(String[] args) {
+        studentinfo student;
+        student = new studentinfo();
+        student.name = "张三";
+        student.height = 172;
+        student.age = 120;
+        System.out.println("学生的名字：" + student.name + "\n" + "身高：" + student.height + "cm" + "\n" + "体重：" + student.age);
+
+        System.out.println();
+
+        studentinfo student2;
+        student2 = new studentinfo();
+        student2.name = "李四";
+        student2.height = 140;
+        student2.age = 280;
+        System.out.println("学生的名字：" + student2.name + "\n" + "身高：" + student2.height + "cm" + "\n" + "体重："
+                + student2.age);
+
+        System.out.println();
+        student.grade();
+        System.out.println("他的学号是" + student.number());
+        System.out.println("他在班级的职位是" + student.post());
+    }
+
+}
---
+++ src/main/java/dt59homework/studentinfo.java
@@ -0,0 +1,38 @@
+/**
+ * Project Name:lession1
+ * File Name:mnb.java
+ * Package Name:lession4
+ * Date:2017年12月23日下午6:07:40
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package lession4;
+
+/**
+ * Description: <br/>
+ * Date: 2017年12月23日 下午6:07:40 <br/>
+ * 
+ * @author 57745
+ * @version
+ * @see
+ */
+public class studentinfo {
+
+    String name;
+
+    int height;
+
+    int age;
+
+    void grade() {
+        System.out.println("二年一班");
+    }
+
+    int number() {
+        return 1;
+    }
+
+    String post() {
+        return "小组长";
+    }
+}
